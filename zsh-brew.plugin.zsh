6d1017832f95aa9998f82330b3d72b54889a092d
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-05 00:41:51 +1100

    feat: add 'site-functions' to the FPATH

--- zsh-brew.plugin.zsh
+++ zsh-brew.plugin.zsh
@@ -16,3 +16,20 @@ if (( ! $+commands[brew] )); then
 
     eval "$("$BREW_PATH" shellenv)"
 fi
+
+# If the 'HOMEBREW_PREFIX' environment variable is not populated then
+# request the prefix from 'brew' and populate
+if [[ -z "$HOMEBREW_PREFIX" ]]; then
+    export HOMEBREW_PREFIX="$(brew --prefix)"
+fi
+
+# Add Homebrew 'site-functions directory to the FPATH
+local HOMEBREW_SITE_FUNCTIONS="$HOMEBREW_PREFIX/share/zsh/site-functions"
+
+if [[ -d "$HOMEBREW_SITE_FUNCTIONS" ]]; then
+    typeset -TUx FPATH fpath
+    fpath=("$HOMEBREW_SITE_FUNCTIONS" $fpath)
+
+    autoload -Uz compinit
+    compinit
+fi
