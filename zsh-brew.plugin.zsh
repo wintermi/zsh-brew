49f471cda893fe1c26357fb232ebd9dac835bcfc
Matthew Winter <33818+wintermi@users.noreply.github.com>
2023-01-06 03:59:43 +1100

    fix: add extra checks before calling brew

--- zsh-brew.plugin.zsh
+++ zsh-brew.plugin.zsh
@@ -14,22 +14,27 @@ if (( ! $+commands[brew] )); then
         fi
     done
 
-    eval "$("$BREW_PATH" shellenv)"
+    # If a path was found, setup the shell environment
+    if [[ ! -z "$BREW_PATH" ]]; then
+        eval "$("$BREW_PATH" shellenv)"
+    fi
 fi
 
-# If the 'HOMEBREW_PREFIX' environment variable is not populated then
-# request the prefix from 'brew' and populate
-if [[ -z "$HOMEBREW_PREFIX" ]]; then
-    export HOMEBREW_PREFIX="$(brew --prefix)"
-fi
+if (( ! $+commands[brew] )); then
+    # If the 'HOMEBREW_PREFIX' environment variable is not populated then
+    # request the prefix from 'brew' and populate
+    if [[ -z "$HOMEBREW_PREFIX" ]]; then
+        export HOMEBREW_PREFIX="$(brew --prefix)"
+    fi
 
-# Add Homebrew 'site-functions directory to the FPATH
-local HOMEBREW_SITE_FUNCTIONS="$HOMEBREW_PREFIX/share/zsh/site-functions"
+    # Add Homebrew 'site-functions directory to the FPATH
+    local HOMEBREW_SITE_FUNCTIONS="$HOMEBREW_PREFIX/share/zsh/site-functions"
 
-if [[ -d "$HOMEBREW_SITE_FUNCTIONS" ]]; then
-    typeset -TUx FPATH fpath
-    fpath=("$HOMEBREW_SITE_FUNCTIONS" $fpath)
+    if [[ -d "$HOMEBREW_SITE_FUNCTIONS" ]]; then
+        typeset -TUx FPATH fpath
+        fpath=("$HOMEBREW_SITE_FUNCTIONS" $fpath)
 
-    autoload -Uz compinit
-    compinit
+        autoload -Uz compinit
+        compinit
+    fi
 fi
