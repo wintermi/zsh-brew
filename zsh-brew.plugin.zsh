b6fc7a9b49e498af0855d9726408031e990176e6
Joe Block <jpb@unixorn.net>
2023-01-05 09:21:26 -0700

    Only set up homebrew site functions when `brew` is found
    - Make setting and using `HOMEBREW_SITE_FUNCTIONS` only happen when `brew` was found, either in `$PATH` or by searching through `DEFAULT_BREW_PATHS`.
    - Unset internal `DEFAULT_BREW_PATHS`, `BREW_PATH` and `HOMEBREW_*` variables after done using them

--- zsh-brew.plugin.zsh
+++ zsh-brew.plugin.zsh
@@ -15,21 +15,29 @@ if (( ! $+commands[brew] )); then
     done
 
     eval "$("$BREW_PATH" shellenv)"
+    # Tidy up environment
+    unset DEFAULT_BREW_PATHS
+    unset BREW_PATH
 fi
 
-# If the 'HOMEBREW_PREFIX' environment variable is not populated then
-# request the prefix from 'brew' and populate
-if [[ -z "$HOMEBREW_PREFIX" ]]; then
-    export HOMEBREW_PREFIX="$(brew --prefix)"
-fi
+if (( $+commands[brew] )); then
+    # If the 'HOMEBREW_PREFIX' environment variable is not populated then
+    # request the prefix from 'brew' and populate
 
-# Add Homebrew 'site-functions directory to the FPATH
-local HOMEBREW_SITE_FUNCTIONS="$HOMEBREW_PREFIX/share/zsh/site-functions"
+    if [[ -z "$HOMEBREW_PREFIX" ]]; then
+        export HOMEBREW_PREFIX="$(brew --prefix)"
+    fi
 
-if [[ -d "$HOMEBREW_SITE_FUNCTIONS" ]]; then
-    typeset -TUx FPATH fpath
-    fpath=("$HOMEBREW_SITE_FUNCTIONS" $fpath)
+    # Add Homebrew 'site-functions directory to the FPATH
+    local HOMEBREW_SITE_FUNCTIONS="$HOMEBREW_PREFIX/share/zsh/site-functions"
 
-    autoload -Uz compinit
-    compinit
+    if [[ -d "$HOMEBREW_SITE_FUNCTIONS" ]]; then
+        typeset -TUx FPATH fpath
+        fpath=("$HOMEBREW_SITE_FUNCTIONS" $fpath)
+        autoload -Uz compinit
+        compinit
+    fi
+    # More tidying up
+    unset HOMEBREW_PREFIX
+    unset HOMEBREW_SITE_FUNCTIONS
 fi
